0e8d11ea0a6d4d0f2730769bd7bd0f18af34a3d8
Reyner Contreras <reynercon@outlook.com>
2018-05-04 23:20:36 -0400

    Añadida pequeña practica hecha para matar el rato (sin luz).

---
+++ Practicas_propias/Voltear_numero/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int numero, auxiliar, tamanyoNumero=1, tamNumDiez, numFinal=0;
+	
+	cout<<"\tVoltear un numero (entero)."<<endl;
+	cout<<"Ingrese numero: "; cin.sync(); cin>>numero;
+	
+	//Determinar el tamaño del numero
+	tamNumDiez=10;
+	while ((numero/tamNumDiez)>0) {
+		tamanyoNumero++;
+		tamNumDiez *= 10;
+	}
+	cout<<"Tamanyo del numero: "<<tamanyoNumero<<endl<<endl;
+	tamNumDiez /= 10;
+	
+	//ahora si, el volteo utilizando variables auxiliares
+	for (int i=tamNumDiez; i>0; i/=10) {
+		auxiliar = (numero%10)*i;
+		numero /= 10;
+		numFinal += auxiliar;
+	}
+	cout<<endl<<"Numero final: "<<numFinal;
+	
+	cin.sync();
+	cin.get();
+	return 0;
+}
